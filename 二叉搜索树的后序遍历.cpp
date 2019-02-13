1a498f92177d531b5d52246f1c4c0846d023af85
fanglliu <liufanglin@zto.cn>
2019-02-13 09:49:55 +0800

    二叉搜索树的后序遍历 更简单解法

--- 二叉搜索树的后序遍历.cpp
+++ 二叉搜索树的后序遍历.cpp
@@ -3,43 +3,30 @@
 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
 */
 
-
 class Solution {
 public:
     bool VerifySquenceOfBST(vector<int> sequence) {
+        if (sequence.empty()) {
+            return false;
+        }
+        
         return VerifySquenceOfBST(sequence.begin(), sequence.end());
     }
     
-    bool VerifySquenceOfBST(vector<int>::iterator start, vector<int>::iterator end) {
-        if (end - start == 0) {
-            return false;
-        }
-        
-        if (end - start <= 2) {
+    bool VerifySquenceOfBST(vector<int>::iterator begin, vector<int>::iterator end) {
+        if (begin == end) {
             return true;
         }
         
-        int rootValue = *(end - 1);
-        auto it = find_if(start, end, [rootValue](auto val) {
-            return val > rootValue;
+        --end;
+        
+        auto it = find_if(begin, end, [=](auto value) {
+            return value > *end ;
         });
         
-        if (it == end ) {
-            return VerifySquenceOfBST(start, end - 1);
-        }
-        
-        if (!all_of(it, end - 1, [rootValue](auto val) { return val >= rootValue; })) {
-            return false;
-        }
-        
-        if (it - start > 0 && !VerifySquenceOfBST(start, it)) {
-            return false;
-        }
-            
-        if (end - 1 - it > 0 && !VerifySquenceOfBST(it, end - 1)) {
-            return false;
-        }
-        
-        return true;
+        return all_of(it, end, [=](auto value) { return value > *end; }) && 
+            VerifySquenceOfBST(begin, it) &&
+            VerifySquenceOfBST(it, end);
     }
+    
 };

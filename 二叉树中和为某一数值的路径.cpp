2fbaeb32fb23b55ca6f75dd06cbcf1a18729068c
fanglliu <liufanglin@zto.cn>
2019-02-21 20:46:39 +0800

    解法2 二叉树中和为某一数值的路径

--- 二叉树中和为某一数值的路径.cpp
+++ 二叉树中和为某一数值的路径.cpp
@@ -1,44 +1,48 @@
 /*
 struct TreeNode {
-	int val;
-	struct TreeNode *left;
-	struct TreeNode *right;
-	TreeNode(int x) :
-			val(x), left(NULL), right(NULL) {
-	}
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
 };*/
 class Solution {
 public:
     vector<vector<int> > FindPath(TreeNode* root,int expectedNumber) {
-        vector<vector<int>> result;
-        int sum = 0;
+        vector<vector<int> > paths;
+        
+        if (root == nullptr) {
+            return paths;
+        }
+        
         vector<int> path;
-        FindPath(root, expectedNumber, path, sum, result);
-        sort(result.begin(), result.end(), [](auto& path1, auto& path2) {
-            return path1.size() > path2.size();
+        FindPath(root, expectedNumber, path, paths);
+        
+        stable_sort(paths.begin(), paths.end(), [](auto path1, auto path2){
+            return path1.size() >= path2.size();
         });
-        return result;
+        
+        return paths;
     }
     
-    void FindPath(TreeNode* root,int expectedNumber, vector<int>& path, int& sum, vector<vector<int>>& result) {
-        if (root == nullptr) {
+    void FindPath(TreeNode* root, int expectedNumber, vector<int> path, vector<vector<int> >& paths) {
+        path.push_back(root->val);
+        if (root->left == nullptr && root->right == nullptr) {
+            if (accumulate(path.begin(), path.end(), 0) == expectedNumber) {
+                paths.push_back(path);
+            }
+            
             return;
         }
         
-        sum += root->val;
-        path.push_back(root->val);
-        
-        if (root->left == nullptr && root->right == nullptr) {
-            if (sum == expectedNumber && path.size() > 0) {
-                result.push_back(path);
-            }
-        } else {
-            FindPath(root->left, expectedNumber, path, sum, result);
-            FindPath(root->right, expectedNumber, path, sum, result);
+        if (root->left) {
+            FindPath(root->left, expectedNumber, path, paths);
         }
         
-        sum -= root->val;
-        path.pop_back();
-
+        if (root->right) {
+            FindPath(root->right, expectedNumber, path, paths);
+        }
     }
+    
 };

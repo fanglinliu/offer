2ce4818a9e025dae8f85f36cc0e561d62be11916
fanglliu <liufanglin@zto.cn>
2019-02-26 21:07:22 +0800

    最小的K个数

--- 最小的K个数.cpp
+++ 最小的K个数.cpp
@@ -3,28 +3,24 @@
 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
 */
 
+#include <cassert>
 
 class Solution {
 public:
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-        if (input.size() < k) {
+        if (k <= 0 || k > input.size() ) {
             return vector<int>();
         }
         
-        priority_queue<int> queue;
-        for (int i = 0; i < input.size(); ++i)
-        {
-        	if (i < k)
-        	{
-	        	queue.push(input[i]);
-        	} else {
-        		if (input[i] < queue.top())
-        		{
-        			queue.pop();
-        			queue.push(input[i]);
-        		}
-        	}
+        make_heap(input.begin(), input.begin() + k);
+        for (int i = k; i < input.size(); i++) {
+            if (input[i] < input.front()) {
+                pop_heap(input.begin(), input.begin() + k);
+                swap(input[i], input[k - 1]);
+                push_heap(input.begin(), input.begin() + k);
+            }
         }
-        return vector<int>(queue.begin(), queue.end());
+
+        return vector<int>(input.begin(), input.begin() + k);
     }
 };
